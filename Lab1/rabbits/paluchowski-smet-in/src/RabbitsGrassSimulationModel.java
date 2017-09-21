a1917eef0d4ed587b5cf9419df8a40a624f0ae81
Mateusz <paluchowski.matthew@gmail.com>
2017-09-21 15:40:10 +0200

    Rabbits first steps

--- Lab1/rabbits/paluchowski-smet-in/src/RabbitsGrassSimulationModel.java
+++ Lab1/rabbits/paluchowski-smet-in/src/RabbitsGrassSimulationModel.java
@@ -1,4 +1,5 @@
 import uchicago.src.sim.engine.Schedule;
+import uchicago.src.sim.engine.SimInit;
 import uchicago.src.sim.engine.SimModelImpl;
 
 /**
@@ -11,36 +12,56 @@ import uchicago.src.sim.engine.SimModelImpl;
  */
 
 
-public class RabbitsGrassSimulationModel extends SimModelImpl {		
+public class RabbitsGrassSimulationModel extends SimModelImpl {
+	
+		private Schedule schedule;
+		private int numRabbits;
 
 		public static void main(String[] args) {
-			
-			System.out.println("Rabbit skeleton");
+			SimInit init = new SimInit();
+			RabbitsGrassSimulationModel model = new RabbitsGrassSimulationModel();
+		    init.loadModel(model, "", false);
+		}
+		
+		public void setup() {
+			// TODO Auto-generated method stub
 			
 		}
 
 		public void begin() {
-			// TODO Auto-generated method stub
-			
+			buildModel();
+			buildSchedule();
+			buildDisplay();
 		}
 
-		public String[] getInitParam() {
-			// TODO Auto-generated method stub
-			return null;
+		public void buildModel(){
+  	    }
+		
+	    public void buildSchedule(){
+		}
+		
+		public void buildDisplay(){
 		}
 
 		public String getName() {
-			// TODO Auto-generated method stub
-			return null;
+			return "RabbitsGrassSimulation";
 		}
 
 		public Schedule getSchedule() {
 			// TODO Auto-generated method stub
-			return null;
+			return schedule;
+		}
+		
+		public String[] getInitParam() {
+			String[] initParams = { "NumRabbits" };
+		    return initParams;
 		}
 
-		public void setup() {
-			// TODO Auto-generated method stub
-			
+		public int getNumRabbits(){
+			return numRabbits;
+		}
+
+		public void setNumRabbits(int numberOfRabbits){
+			numRabbits = numberOfRabbits;
 		}
 }
