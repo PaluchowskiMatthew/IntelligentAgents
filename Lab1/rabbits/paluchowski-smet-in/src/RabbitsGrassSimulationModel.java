9d46219bd6d71c618f4f5be251c0cefef9e30a45
Mateusz <paluchowski.matthew@gmail.com>
2017-09-27 17:00:02 +0200

    slighlty more

--- Lab1/rabbits/paluchowski-smet-in/src/RabbitsGrassSimulationModel.java
+++ Lab1/rabbits/paluchowski-smet-in/src/RabbitsGrassSimulationModel.java
@@ -1,67 +1,126 @@
 import uchicago.src.sim.engine.Schedule;
 import uchicago.src.sim.engine.SimInit;
 import uchicago.src.sim.engine.SimModelImpl;
+import uchicago.src.sim.gui.DisplaySurface;
 
 /**
- * Class that implements the simulation model for the rabbits grass
- * simulation.  This is the first class which needs to be setup in
- * order to run Repast simulation. It manages the entire RePast
- * environment and the simulation.
+ * Class that implements the simulation model for the rabbits grass simulation.
+ * This is the first class which needs to be setup in order to run Repast
+ * simulation. It manages the entire RePast environment and the simulation.
  *
- * @author 
+ * @author
  */
 
-
 public class RabbitsGrassSimulationModel extends SimModelImpl {
-	
-		private Schedule schedule;
-		private int numRabbits;
 
-		public static void main(String[] args) {
-			SimInit init = new SimInit();
-			RabbitsGrassSimulationModel model = new RabbitsGrassSimulationModel();
-		    init.loadModel(model, "", false);
-		}
-		
-		public void setup() {
-			// TODO Auto-generated method stub
-			
-		}
+	private Schedule schedule;
 
-		public void begin() {
-			buildModel();
-			buildSchedule();
-			buildDisplay();
-		}
+	private RabbitsGrassSimulationSpace rabbitsGrassSpace;
 
-		public void buildModel(){
-  	    }
-		
-	    public void buildSchedule(){
-		}
-		
-		public void buildDisplay(){
-		}
+	private DisplaySurface displaySurface;
 
-		public String getName() {
-			return "RabbitsGrassSimulation";
-		}
+	// Default Values
+	private static final int NUMRABBITS = 50;
+	private static final int WORLDXSIZE = 20;
+	private static final int WORLDYSIZE = 20;
+	private static final int TOTALGRASS = 100;
 
-		public Schedule getSchedule() {
-			// TODO Auto-generated method stub
-			return schedule;
-		}
-		
-		public String[] getInitParam() {
-			String[] initParams = { "NumRabbits" };
-		    return initParams;
-		}
+	private int numRabbits = NUMRABBITS;
+	private int worldXSize = WORLDXSIZE;
+	private int worldYSize = WORLDYSIZE;
+	private int grass = TOTALGRASS;
 
-		public int getNumRabbits(){
-			return numRabbits;
-		}
+	public static void main(String[] args) {
+		SimInit init = new SimInit();
+		RabbitsGrassSimulationModel model = new RabbitsGrassSimulationModel();
+		init.loadModel(model, "", false);
+	}
 
-		public void setNumRabbits(int numberOfRabbits){
-			numRabbits = numberOfRabbits;
+	public void setup() {
+		// TODO Auto-generated method stub
+		System.out.println("Running setup");
+		rabbitsGrassSpace = null;
+
+		if (displaySurface != null) {
+			displaySurface.dispose();
 		}
+		displaySurface = null;
+
+		displaySurface = new DisplaySurface(this, "Rabbits Grass Simulation Model Window 1");
+
+		registerDisplaySurface("Rabbits Grass Simulation Model Window 1", displaySurface);
+	}
+
+	public void begin() {
+		buildModel();
+		buildSchedule();
+		buildDisplay();
+	}
+
+	public void buildModel() {
+		System.out.println("Running BuildModel");
+		rabbitsGrassSpace = new RabbitsGrassSimulationSpace(worldXSize, worldYSize);
+		rabbitsGrassSpace.spreadGrass(grass);
+	}
+
+	public void buildSchedule() {
+		System.out.println("Running BuildSchedule");
+	}
+
+	public void buildDisplay() {
+		System.out.println("Running BuildDisplay");
+	}
+
+	public String getName() {
+		return "RabbitsGrassSimulation";
+	}
+
+	public Schedule getSchedule() {
+		// TODO Auto-generated method stub
+		return schedule;
+	}
+
+	// Sliders for:
+	// Grid size
+	// The number of rabbits defines the initial number of rabbits
+	// The birth threshold of rabbits defines the energy level at which the rabbit
+	// reproduces.
+	// The grass growth rate controls the rate at which grass grows (total amount of
+	// grass added to the whole world within one simulation tick).
+	public String[] getInitParam() {
+		String[] initParams = { "NumRabbits", "WorldXSize", "WorldYSize", "Grass" };
+		return initParams;
+	}
+
+	public int getNumRabbits() {
+		return numRabbits;
+	}
+
+	public void setNumRabbits(int numberOfRabbits) {
+		numRabbits = numberOfRabbits;
+	}
+
+	public int getWorldXSize() {
+		return worldXSize;
+	}
+
+	public void setWorldXSize(int wxs) {
+		worldXSize = wxs;
+	}
+
+	public int getWorldYSize() {
+		return worldYSize;
+	}
+
+	public void setWorldYSize(int wys) {
+		worldYSize = wys;
+	}
+
+	public int getGrass() {
+		return grass;
+	}
+
+	public void setGrass(int g) {
+		grass = g;
+	}
 }
--- Lab1/rabbits/paluchowski-smet-in/src/RabbitsGrassSimulationSpace.java
+++ Lab1/rabbits/paluchowski-smet-in/src/RabbitsGrassSimulationSpace.java
@@ -1,8 +1,46 @@
+
 /**
  * Class that implements the simulation space of the rabbits grass simulation.
  * @author 
  */
+import uchicago.src.sim.space.Object2DGrid;
 
 public class RabbitsGrassSimulationSpace {
+	private Object2DGrid grassSpace;
 
+	// initialize the space object by filling each cell with an Integer object, with
+	// the initial value of zero
+	public RabbitsGrassSimulationSpace(int xSize, int ySize) {
+		grassSpace = new Object2DGrid(xSize, ySize);
+		for (int i = 0; i < xSize; i++) {
+			for (int j = 0; j < ySize; j++) {
+				grassSpace.putObjectAt(i, j, new Integer(0));
+			}
+		}
+	}
+
+	// Randomly place grass in grassSpace
+	public void spreadGrass(int grass) {
+		for (int i = 0; i < grass; i++) {
+
+			// Choose coordinates
+			int x = (int) (Math.random() * (grassSpace.getSizeX()));
+			int y = (int) (Math.random() * (grassSpace.getSizeY()));
+
+			// Get the value of the object at those coordinates
+			int currentValue = getGrassAt(x, y);
+			// Replace the Integer object with another one with the new value
+			grassSpace.putObjectAt(x, y, new Integer(currentValue + 1));
+		}
+	}
+
+	public int getGrassAt(int x, int y) {
+		int i;
+		if (grassSpace.getObjectAt(x, y) != null) {
+			i = ((Integer) grassSpace.getObjectAt(x, y)).intValue();
+		} else {
+			i = 0;
+		}
+		return i;
+	}
 }
