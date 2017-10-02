c6ee3791144201ebff2058292446d7a9594226f0
vsmet <vincent.smet@epfl.ch>
2017-10-02 16:24:07 +0200

    Update

--- Lab1/rabbits/paluchowski-smet-in/src/RabbitsGrassSimulationAgent.java
+++ Lab1/rabbits/paluchowski-smet-in/src/RabbitsGrassSimulationAgent.java
@@ -1,5 +1,8 @@
+import java.awt.Color;
+
 import uchicago.src.sim.gui.Drawable;
 import uchicago.src.sim.gui.SimGraphics;
+import uchicago.src.sim.space.Object2DGrid;
 
 /**
  * Class that implements the simulation agent for the rabbits grass simulation.
@@ -8,20 +11,104 @@ import uchicago.src.sim.gui.SimGraphics;
  */
 
 public class RabbitsGrassSimulationAgent implements Drawable {
-
-	public void draw(SimGraphics arg0) {
-		// TODO Auto-generated method stub
-
+	private int x;  
+	private int y;
+	private int vX;
+	private int vY;
+	private int energy;
+	private int bt;
+	private static int IDNumber = 0;
+	private int ID;
+	private RabbitsGrassSimulationSpace rabbitsGrassSpace;
+	
+	public RabbitsGrassSimulationAgent(int birthTreshhold){
+	    x = -1;
+	    y = -1;
+	    energy = 50;
+	    setVxVy();
+	    bt = birthTreshhold;
+	    IDNumber++;
+	    ID = IDNumber;
 	}
+	
+	private void setVxVy(){
+	    vX = 0;
+	    vY = 0;
+	    int v = 0;
+	    while(v == 0){
+	      v = (int)Math.floor(Math.random() * 3) - 1;
+	    }
+	    int dir = (int)Math.floor(Math.random()*2);
+	    if(dir == 0) {
+	    		vX = v;
+	    }
+	    	else {
+	    		vY = v;
+	    }
+	  }
+    public void setXY(int newX, int newY){
+        x = newX;
+        y = newY;
+    }
+
+    public void setRabbitsGrassSimulationSpace(RabbitsGrassSimulationSpace rgs){
+        rabbitsGrassSpace = rgs;
+      }
+    
+	public void draw(SimGraphics G) {
+		G.drawFastRoundRect(Color.white);
+	}
+		
 
 	public int getX() {
-		// TODO Auto-generated method stub
-		return 0;
+		return x;
 	}
 
 	public int getY() {
-		// TODO Auto-generated method stub
-		return 0;
+		return y;
 	}
 
+	public String getID(){
+	    return "A-" + ID;
+	  }
+
+	  public int getEnergy(){
+	    return energy;
+	  }
+	  
+	  public int getbt(){
+		    return bt;
+		  }
+
+
+	  
+
+	  public void report(){
+	    System.out.println(getID() + 
+	                       " at " + 
+	                       x + ", " + y + 
+	                       " has an energy level of " + 
+	                       getEnergy() );
+	  }
+	  public void step(){
+		  int newX = x + vX;
+		    int newY = y + vY;
+
+		    Object2DGrid grid = rabbitsGrassSpace.getCurrentAgentSpace();
+		    newX = (newX + grid.getSizeX()) % grid.getSizeX();
+		    newY = (newY + grid.getSizeY()) % grid.getSizeY();
+
+		    if(tryMove(newX, newY)){
+		      energy += rabbitsGrassSpace.eatGrass(x, y) - 1;
+		    }
+		    else{
+		      setVxVy();
+		    }
+	  }
+	  private boolean tryMove(int newX, int newY){
+		    return rabbitsGrassSpace.moveAgentAt(x, y, newX, newY);
+		  }
+	  public void reproduce() {
+		  energy = 50;
+	  }
 }
--- Lab1/rabbits/paluchowski-smet-in/src/RabbitsGrassSimulationModel.java
+++ Lab1/rabbits/paluchowski-smet-in/src/RabbitsGrassSimulationModel.java
@@ -1,11 +1,19 @@
 import java.awt.Color;
+import java.util.ArrayList;
 
+
+import uchicago.src.sim.analysis.DataSource;
+import uchicago.src.sim.analysis.OpenSequenceGraph;
+import uchicago.src.sim.analysis.Sequence;
+import uchicago.src.sim.engine.BasicAction;
 import uchicago.src.sim.engine.Schedule;
 import uchicago.src.sim.engine.SimInit;
 import uchicago.src.sim.engine.SimModelImpl;
 import uchicago.src.sim.gui.DisplaySurface;
 import uchicago.src.sim.gui.ColorMap;
+import uchicago.src.sim.gui.Object2DDisplay;
 import uchicago.src.sim.gui.Value2DDisplay;
+import uchicago.src.sim.util.SimUtilities;
 
 /**
  * Class that implements the simulation model for the rabbits grass simulation.
@@ -27,20 +35,38 @@ public class RabbitsGrassSimulationModel extends SimModelImpl {
 	private Schedule schedule;
 
 	private RabbitsGrassSimulationSpace rabbitsGrassSpace;
+	
+	private ArrayList agentList;
 
 	private DisplaySurface displaySurface;
+	
+	private OpenSequenceGraph amountOfGrassInSpace;
+	
+	class moneyInSpace implements DataSource, Sequence {
+
+	    public Object execute() {
+	      return new Double(getSValue());
+	    }
+
+	    public double getSValue() {
+	      return (double)cdSpace.getTotalMoney();
+	    }
+	  }
 
 	// Default Values
 	private static final int NUMRABBITS = 50;
 	private static final int WORLDXSIZE = 20;
 	private static final int WORLDYSIZE = 20;
-	private static final int TOTALGRASS = 100;
+	private static final int TOTALGRASS = 50;
+	private static final int BIRTHTRESHHOLD = 70;
+
 
 	private int numRabbits = NUMRABBITS;
 	private int worldXSize = WORLDXSIZE;
 	private int worldYSize = WORLDYSIZE;
 	private int grass = TOTALGRASS;
-
+	private int birthTreshhold = BIRTHTRESHHOLD; 
+	
 	public static void main(String[] args) {
 		SimInit init = new SimInit();
 		RabbitsGrassSimulationModel model = new RabbitsGrassSimulationModel();
@@ -51,6 +77,8 @@ public class RabbitsGrassSimulationModel extends SimModelImpl {
 		// TODO Auto-generated method stub
 		System.out.println("Running setup");
 		rabbitsGrassSpace = null;
+		agentList = new ArrayList();
+		schedule = new Schedule(1);
 
 		if (displaySurface != null) {
 			displaySurface.dispose();
@@ -74,10 +102,46 @@ public class RabbitsGrassSimulationModel extends SimModelImpl {
 		System.out.println("Running BuildModel");
 		rabbitsGrassSpace = new RabbitsGrassSimulationSpace(worldXSize, worldYSize);
 		rabbitsGrassSpace.spreadGrass(grass);
+		
+		for(int i = 0; i < numRabbits; i++){
+		      addNewAgent();
+		}
+		for(int i = 0; i < agentList.size(); i++){
+		      RabbitsGrassSimulationAgent ra = (RabbitsGrassSimulationAgent)agentList.get(i);
+		      ra.report();
+		}
+		 
 	}
 
 	public void buildSchedule() {
 		System.out.println("Running BuildSchedule");
+		
+		class RabbitStep extends BasicAction {
+		      public void execute() {
+		        SimUtilities.shuffle(agentList);
+		        for(int i =0; i < agentList.size(); i++){
+		          RabbitsGrassSimulationAgent ra = (RabbitsGrassSimulationAgent)agentList.get(i);
+		          ra.step();
+		        }
+		      int reproduction = reproduceAgents();
+		      for(int i=0; i < reproduction; i++) {
+		    	  	addNewAgent();
+		      }
+		      int deadAgents = reapDeadAgents();
+		      rabbitsGrassSpace.spreadGrass(grass);
+		        displaySurface.updateDisplay();
+		      }
+	    	}
+
+		schedule.scheduleActionBeginning(0, new RabbitStep());
+		
+		class RabbitCountLiving extends BasicAction {
+		      public void execute(){
+		        countLivingAgents();
+		      }
+		    }
+
+		    schedule.scheduleActionAtInterval(10, new RabbitCountLiving());
 	}
 
 	public void buildDisplay() {
@@ -88,17 +152,64 @@ public class RabbitsGrassSimulationModel extends SimModelImpl {
 		for (int i = 1; i < 16; i++) {
 			map.mapColor(i, new Color(0, (int) (i * 8 + 127), 0));
 		}
-		map.mapColor(0, Color.white);
+		map.mapColor(0, Color.black);
 
-		Value2DDisplay displayMoney = new Value2DDisplay(rabbitsGrassSpace.getCurrentGrassSpace(), map);
+		Value2DDisplay displayGrass = new Value2DDisplay(rabbitsGrassSpace.getCurrentGrassSpace(), map);
 
-		displaySurface.addDisplayable(displayMoney, "Grass");
+		Object2DDisplay displayAgents = new Object2DDisplay(rabbitsGrassSpace.getCurrentAgentSpace());
+	    displayAgents.setObjectList(agentList);
+		
+		displaySurface.addDisplayableProbeable(displayGrass, "Grass");
+		displaySurface.addDisplayableProbeable(displayAgents, "Agents");
 
 	}
 
 	public String getName() {
 		return "RabbitsGrassSimulation";
 	}
+
+	private void addNewAgent(){
+	    RabbitsGrassSimulationAgent a = new RabbitsGrassSimulationAgent(birthTreshhold);
+	    agentList.add(a);
+	    rabbitsGrassSpace.addAgent(a);
+	}
+	
+	private int reapDeadAgents(){
+	    int count = 0;
+	    for(int i = (agentList.size() - 1); i >= 0 ; i--){
+	      RabbitsGrassSimulationAgent ra = (RabbitsGrassSimulationAgent)agentList.get(i);
+	      if(ra.getEnergy() < 1){
+	        rabbitsGrassSpace.removeAgentAt(ra.getX(), ra.getY());
+	        agentList.remove(i);
+	        count++;
+	      }
+	    }
+	    return count;
+	  }
+	
+	private int reproduceAgents() {
+		int count = 0;
+		for(int i = 0; i < agentList.size(); i++){
+			RabbitsGrassSimulationAgent ra = (RabbitsGrassSimulationAgent)agentList.get(i);
+		    if(ra.getEnergy() > ra.getbt()) {
+		    		count++;
+		    		ra.reproduce();
+		    }
+		}
+		return count;
+	}
+	
+	
+	private int countLivingAgents(){
+	    int livingAgents = 0;
+	    for(int i = 0; i < agentList.size(); i++){
+	      RabbitsGrassSimulationAgent ra = (RabbitsGrassSimulationAgent)agentList.get(i);
+	      if(ra.getEnergy() > 0) livingAgents++;
+	    }
+	    System.out.println("Number of living agents is: " + livingAgents);
+
+	    return livingAgents;
+	  }
 
 	public Schedule getSchedule() {
 		// TODO Auto-generated method stub
@@ -113,7 +224,7 @@ public class RabbitsGrassSimulationModel extends SimModelImpl {
 	// The grass growth rate controls the rate at which grass grows (total amount of
 	// grass added to the whole world within one simulation tick).
 	public String[] getInitParam() {
-		String[] initParams = { "NumRabbits", "WorldXSize", "WorldYSize", "Grass" };
+		String[] initParams = { "NumRabbits", "WorldXSize", "WorldYSize", "Grass", "birthTreshhold_stat"};
 		return initParams;
 	}
 
@@ -148,4 +259,13 @@ public class RabbitsGrassSimulationModel extends SimModelImpl {
 	public void setGrass(int g) {
 		grass = g;
 	}
+	
+	public int getbirthTreshhold() {
+		return birthTreshhold;
+	}
+
+	public void setbirthTreshhold(int bt) {
+		birthTreshhold = bt;
+	}
+	
 }
